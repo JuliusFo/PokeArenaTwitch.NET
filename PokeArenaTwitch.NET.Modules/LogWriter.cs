17979e127d4f7064444331ea005fe2515aebae31
Julius Forner <j.forner@ccc-software.de>
2023-03-21 23:54:03 +0100

    Added basic console logger.

---
+++ PokeArenaTwitch.NET.Modules/LogWriter.cs
@@ -0,0 +1,27 @@
+﻿namespace PokeArenaTwitch.NET.Modules
+{
+    public static class LogWriter
+    {
+        private static void WriteLog(string preamble, ConsoleColor color, string logText)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(preamble + ":" + logText);
+            Console.ResetColor();
+        }
+
+        public static void Log(string Text)
+        {
+            WriteLog("[INFO]", ConsoleColor.White, Text);
+        }
+
+        public static void LogWarning(string Text)
+        {
+            WriteLog("[WARNING]", ConsoleColor.Yellow, Text);
+        }
+
+        public static void LogError(string Text)
+        {
+            WriteLog("[ERROR]", ConsoleColor.Red, Text);
+        }
+    }
+}
