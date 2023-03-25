d46b19ca63c37cb9a7005478e62118d840304dd8
Julius Forner <j.forner@ccc-software.de>
2023-03-25 17:06:01 +0100

    Added dependency injection.

--- PokeArenaTwitch.NET/Program.cs
+++ PokeArenaTwitch.NET/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PokeArenaTwitch.NET.Data;
+using PokeArenaTwitch.NET.Services.Pokemon;
+using PokeArenaTwitch.NET.Services.Twitch;
+
+namespace PokeArenaTwitch.NET // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddDbContext<AppDbContext>();
+                services.AddScoped<TwitchUserService>();
+                services.AddScoped<TwitchAccessService>();
+                services.AddScoped<PokemonMasterDataService>();
+            })
+            .Build();
+
+            Console.WriteLine("Test-Run");
+        }
+    }
+}
